106b401a24aa36f2e53241e4b72e74a25dd54a87
Durgan <durganmcbroom@gmail.com>
2021-06-26 09:50:43 -0700

    Fixed tests

--- spring.server/src/main/java/module-info.java
+++ spring.server/src/main/java/module-info.java
@@ -1,5 +1,4 @@
 module yakclient.websocket.messaging.spring.server {
     requires kotlin.stdlib;
     requires transitive yakclient.websocket.messaging.spring.common;
-    requires spring.websocket;
 }
